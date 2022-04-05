b6953d61565105f206a2be77e6f8851fe81d4f0e
edogalihr <edogalihr7@gmail.com>
2022-04-05 23:15:32 +0700

    edo

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
+/*edo
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
 |--------------------------------------------------------------------------
